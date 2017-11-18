c0dad25d1d77bf5363293b04ebabb6d0fae95915
Nadyap <nadyaprabaningrum@gmail.com>
2017-11-18 20:59:19 +0700

    update province

--- application/views/cek_ongkir.php
+++ application/views/cek_ongkir.php
@@ -2,16 +2,13 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Novely</title>
+	<title>Demo Cek Raja Ongkir</title>
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/tether.min.css">
-	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-3.2.1.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/tether.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 </head>
 <body>
 <nav class="navbar navbar-default">
   <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
@@ -19,63 +16,86 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Cek Ongkir</a>
+      <a class="navbar-brand" href="#">Cek Raja Ongkir</a>
     </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-		<ul class="nav navbar-nav">
-        <li class="active"><a href="http://www.malasngoding.com">Home <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Profil</a></li>
-        <li><a href="#">Tentang Kami</a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Tutorial
-                        <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">HTML</a></li>
-            <li><a href="#">CSS</a></li>
-            <li><a href="#">Bootstrap</a></li>
-          </ul>
-        </li>
+        <li class="active"><a href="http://surdaysoft.blogspot.com">Blog <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        
       </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Daftar</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
+        <li><a href="#">Link</a></li>
+        
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
 <div class="wrapper">
+<div class="row">
+	<div class="col-md-12">
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- test -->
+		<ins class="adsbygoogle"
+		     style="display:block"
+		     data-ad-client="ca-pub-1762934784326172"
+		     data-ad-slot="6122588643"
+		     data-ad-format="auto"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+	</div>
+</div>
 	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="panel panel-info">
+				  <div class="panel-heading">
+				    <h3 class="panel-title">Asal</h3>
+				  </div>
+				  <div class="panel-body">
+				    <select class="form-control" name="propinsi_asal" id="propinsi_asal">
+						<option value="" selected="" disabled="">Pilih Provinsi</option>
+						<?php $this->load->view('rajaongkir/getProvince'); ?>
+					</select>
+					<br>
+					<select class="form-control" name="origin" id="origin">
+						<option value="" selected="" disabled="">Pilih Kota</option>
+					</select>
+				  </div>
+				</div>
+			</div>
+			
+			<div class="col-md-6">
+				<div class="panel panel-success">
+				  <div class="panel-heading">
+				    <h3 class="panel-title">Tujuan</h3>
+				  </div>
+				  <div class="panel-body">
+				    <select class="form-control" name="propinsi_tujuan" id="propinsi_tujuan">
+						<option value="" selected="" disabled="">Pilih Provinsi</option>
+						<?php $this->load->view('rajaongkir/getProvince'); ?>
+					</select>
+					<br>
+					<select class="form-control" name="destination" id="destination">
+						<option value="" selected="" disabled="">Pilih Kota</option>
+					</select>
+				  </div>
+				</div>
+			</div>
+		</div>
+
 		<div class="row">
 			<div class="col-md-4">
 				<div class="panel panel-success">
 				  <div class="panel-heading">
-				    <h3 class="panel-title">Pengiriman</h3>
+				    <h3 class="panel-title">Berat</h3>
 				  </div>
 				  <div class="panel-body">
-				  <span> Asal Kota</span>
-					<select class="form-control" name="origin" id="origin">
-						<option value="" selected="" disabled="">Pilih Kota</option>
-						<?php $this->load->view('rajaongkir/getCity'); ?>
-					</select>
-					<br>
-					<span> Tujuan Kota</span>
-					<select class="form-control" name="destination" id="destination">
-						<option value="" selected="" disabled="">Pilih Kota</option>
-						<?php $this->load->view('rajaongkir/getCity'); ?>
-					</select>
-					<br>
-				  <Span>Berat Barang(gram) :
 				    <input type="text" name="berat" placeholder="gram" id="berat" class="form-control">
 				    <br>
-					<span>Pilih Kurir</span>
 				    <select class="form-control" name="courier" id="courier">
 				    	<option value="">Pilih Kurir</option>
 				    	<option value="jne">JNE</option>
@@ -98,6 +118,8 @@
 					          type: "GET",
 					          data : {origin: w, destination: x, berat: y, courier: z},
 					          success: function (ajaxData){
+					              //$('#tombol_export').show();
+					              //$('#hasilReport').show();
 					              $("#hasil").html(ajaxData);
 					          }
 					      });
@@ -113,7 +135,7 @@
 				  <div class="panel-body">
 				  	<ol>
 					    <div id="hasil">
-
+					    	
 					    </div>
 				    </ol>
 				  </div>
@@ -124,5 +146,38 @@
 </div>
 
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/JQuery.min.js"></script>
+
+<script>
+$(document).ready(function(){
+
+	$("#propinsi_asal").click(function(){
+		$.post("<?php echo base_url(); ?>index.php/rajaongkir/getCity/"+$('#propinsi_asal').val(),function(obj){
+			$('#origin').html(obj);
+		});
+			
+	});
+
+	$("#propinsi_tujuan").click(function(){
+		$.post("<?php echo base_url(); ?>index.php/rajaongkir/getCity/"+$('#propinsi_tujuan').val(),function(obj){
+			$('#destination').html(obj);
+		});
+			
+	});
+
+	/*
+	$("#cari").click(function(){
+		$.post("<?php echo base_url(); ?>index.php/rajaongkir/getCost/"+$('#origin').val()+'&dest='+$('#destination').val()+'&berat='+$('#berat').val()+'&courier='+$('#courier').val(),function(obj){
+			$('#hasil').html(obj);
+		});
+	});
+
+	*/
+
+	
+});
+</script>
 </body>
 </html>
+
+
+
