cac48db0591b0aa23c89be594f2d96558e2f7d23
Abdurrahman <capteinn@gmail.com>
2017-11-19 14:17:54 +0700

    perubahan getCost konflik

--- application/views/rajaongkir/getCost.php
+++ application/views/rajaongkir/getCost.php
@@ -36,30 +36,30 @@ if ($err) {
 <?php  for ($k=0; $k < count($data['rajaongkir']['results']); $k++) { ?>
 	 <div title="<?php echo strtoupper($data['rajaongkir']['results'][$k]['name']);?>" style="padding:10px">
 		 <table class="table table-striped">
-			 <tr>
+			<tr>
 				 <th>No.</th>
 				 <th>Jenis Layanan</th>
 				 <th>Estimasi</th>
 				 <th>Tarif</th>
-			 </tr>
-			 <?php for ($l=0; $l < count($data['rajaongkir']['results'][$k]['costs']); $l++) { ?>
-			 <tr>
-				 <td><?php echo $l+1;?></td>
-				 <td>
-					 <div style="font:bold 16px Arial">
-             <?php echo $data['rajaongkir']['results'][$k]['costs'][$l]['service'];?>
-           </div>
-					 <div style="font:normal 11px Arial">
-             <?php echo $data['rajaongkir']['results'][$k]['costs'][$l]['description'];?>
-           </div>
-				 </td>
-				 <td align="center">&nbsp;
-           <?php echo $data['rajaongkir']['results'][$k]['costs'][$l]['cost'][0]['etd'];?> days
-         </td>
-				 <td align="right">
-           <?php echo number_format($data['rajaongkir']['results'][$k]['costs'][$l]['cost'][0]['value']);?>
-         </td>
-			 </tr>
+			</tr>
+			<?php for ($l=0; $l < count($data['rajaongkir']['results'][$k]['costs']); $l++) { ?>
+			<tr>
+				<td><?php echo $l+1;?></td>
+				<td>
+					<div style="font:bold 16px Arial">
+						<?php echo $data['rajaongkir']['results'][$k]['costs'][$l]['service'];?>
+					</div>
+					<div style="font:normal 11px Arial">
+						<?php echo $data['rajaongkir']['results'][$k]['costs'][$l]['description'];?>
+					</div>
+				</td>
+				<td align="center">&nbsp;
+					<?php echo $data['rajaongkir']['results'][$k]['costs'][$l]['cost'][0]['etd'];?> days
+				</td>
+				<td align="right">
+					<?php echo number_format($data['rajaongkir']['results'][$k]['costs'][$l]['cost'][0]['value']);?>
+				</td>
+			</tr>
 			 <?php
 			 }
 			 ?>
