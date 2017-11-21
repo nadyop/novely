c5b9ea575063255e3f793304ca8afca56264a1e7
Nadyap <nadyaprabaningrum@gmail.com>
2017-11-21 09:07:31 +0700

    update post to twitter

--- application/controllers/twitter.php
+++ application/controllers/twitter.php
@@ -3,13 +3,6 @@
 require APPPATH . '/libraries/BaseController.php';
 require APPPATH . '/libraries/TwitterAPIExchange.php';
 
-/**
- * Class : User (UserController)
- * User Class to control all user related operations.
- * @author : Kishor Mali
- * @version : 1.1
- * @since : 15 November 2016
- */
 class Twitter extends BaseController
 {
     /**
@@ -19,19 +12,19 @@ class Twitter extends BaseController
     {
         parent::__construct();
         $this->load->model('twitter_model');
-        $this->isLoggedIn();   
+        $this->isLoggedIn();
     }
-    
+
     /**
      * This function used to load the first screen of the user
      */
     public function index()
     {
-        $this->global['pageTitle'] = 'CodeInsect : Dashboard';
+        $this->global['pageTitle'] = 'NOVELY';
 
         $this->loadViews("twitter", $this->global, NULL , NULL);
     }
-    
+
     /**
      * This function is used to add new user to the system
      */
@@ -39,7 +32,7 @@ class Twitter extends BaseController
     {
 		// reference
 		// https://dev.twitter.com/rest/reference
-		
+
 		ini_set('display_errors', 1);
 
 		$settings = array(
@@ -51,7 +44,7 @@ class Twitter extends BaseController
 
 		// end point URL for POST
 		$url = 'https://api.twitter.com/1.1/statuses/update.json';
-	
+
         if($this->isAdmin() == TRUE)
         {
             $this->loadThis();
@@ -59,10 +52,10 @@ class Twitter extends BaseController
         else
         {
             $this->load->library('form_validation');
-            
+
             $this->form_validation->set_rules('title','required|max_length[128]|xss_clean');
             $this->form_validation->set_rules('isi','required|max_length[128]|xss_clean');
-            
+
             if($this->form_validation->run() == FALSE)
             {
                 $this->addNew();
@@ -71,16 +64,17 @@ class Twitter extends BaseController
             {
                 $judul = $this->input->post('judul');
                 $isi = $this->input->post('isi');
-				$content = $judul+"\n"+$isi;
-				
+				        $content = $judul."\n".$isi;
+
+                // kirim ke database nya
                 $postInfo = array('judul'=>$judul, 'isi'=>$isi);
-                
+
                 $this->load->model('twitter_model');
                 $result = $this->twitter_model->addNewPost($postInfo);
-                
-				// post field
+
+				// post field ke twitter
 				$postfields = array(
-					'status' => "$isi"
+					'status' => "$content"
 				);
 
 				// methodnya
@@ -89,9 +83,9 @@ class Twitter extends BaseController
 				$twitter = new TwitterAPIExchange($settings);
 				$result = $twitter->buildOauth($url, $requestMethod)
 							 ->setPostfields($postfields)
-							 ->performRequest();			 
+							 ->performRequest();
 				// print_r($result);
-				
+
                 if($result > 0)
                 {
                     $this->session->set_flashdata('success', 'New Post created successfully');
@@ -100,12 +94,12 @@ class Twitter extends BaseController
                 {
                     $this->session->set_flashdata('error', 'Post creation failed');
                 }
-                
+
                 redirect('twitter');
             }
         }
     }
-	
+
 	    /**
      * This function is used to load the add new form
      */
@@ -126,7 +120,7 @@ class Twitter extends BaseController
     function pageNotFound()
     {
         $this->global['pageTitle'] = 'CodeInsect : 404 - Page Not Found';
-        
+
         $this->loadViews("404", $this->global, NULL, NULL);
     }
 }
